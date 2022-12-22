f667d706752062a08d3fccda51be906c280d9fea
Siva - Eunimart <115470016+siva3io@users.noreply.github.com>
2022-12-23 01:45:13 +0530

    Update env.go

--- backend_core/pkg/util/env.go
+++ backend_core/pkg/util/env.go
@@ -23,7 +23,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-const projectDirName = "fermion"
+const projectDirName = "fermion-backend"
 
 type Env interface {
 	GetString(name string) string
